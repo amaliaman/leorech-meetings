96eebe864996bb0b4c825d2da66487b5224f24ab
Amalia Man <amaliagilad@gmail.com>
2018-12-13 16:11:20 +0200

    Convert meeting table to bootstrap elements

--- src/components/main/MeetingsTable.js
+++ src/components/main/MeetingsTable.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
-import { Table, ListGroup, ListGroupItem } from 'reactstrap';
+import {
+    Table, ListGroup, ListGroupItem, Card, Button, CardImg, CardTitle, CardText, CardColumns, CardHeader,
+    CardSubtitle, CardBody
+} from 'reactstrap';
 
 import CustomLoader from '../general/CustomLoader';
 import { MobileView, DefaultView } from '../general/ResponsiveWrappers';
@@ -16,14 +19,15 @@ import stringUtils from '../../utils/StringUtils';
 class MeetingsTable extends Component {
     render() {
         const headers = Object.values(tableHeaders);
-
+        const textColor = `text-${bsColors.INFO}`;
         return (
             <CustomLoader isLoading={this.props.isLoading}>
+
                 <DefaultView>
                     <Table responsive borderless striped size='sm'>
                         <thead>
                             <tr>
-                                {headers.map((h, i) => <th key={i} className={`text-${bsColors.INFO}`}>{h}</th>)}
+                                {headers.map((h, i) => <th key={i} className={textColor}>{h}</th>)}
                             </tr>
                         </thead>
                         <tbody>
@@ -41,42 +45,29 @@ class MeetingsTable extends Component {
                 </DefaultView>
 
                 <MobileView>
-                    <ListGroup>
+                    <CardColumns>
                         {this.props.meetings.map(m => (
-                            <ListGroupItem key={m.id}>{/* make grid!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */}
-                                <div>{tableHeaders.THERAPIST}: {m.therapist}</div>
-                                <div>{tableHeaders.PATIENT}: {m.patient}</div>
-                                <div>{tableHeaders.DEPARTMENT}: {m.department.name}</div>
-                                <div>{tableHeaders.ATTENDEE}: {m.attendee.name}</div>
-                                <div>{tableHeaders.REPORT_DATE}: {stringUtils.getDateString(new Date(m.reportDate))}</div>
-                            </ListGroupItem>
+                            <Card key={m.id} outline color={bsColors.INFO}>
+                                <CardHeader>
+                                    {stringUtils.getDateString(new Date(m.reportDate))}, {m.therapist}
+                                </CardHeader>
+                                <CardBody>
+                                    {/* <CardTitle>{m.patient}</CardTitle>
+                                    <CardSubtitle>{m.therapist}</CardSubtitle> */}
+                                    <CardText>
+                                        <span className={textColor}>{tableHeaders.THERAPIST}</span>: <span>{m.therapist}</span>,
+                                        <span className={textColor}>{tableHeaders.PATIENT}</span>: <span>{m.patient}</span>
+                                        <br />
+                                        <span className={textColor}>{tableHeaders.DEPARTMENT}</span>: <span>{m.department.name}</span>,
+                                        <span className={textColor}>{tableHeaders.ATTENDEE}</span>: <span>{m.attendee.name}</span>
+                                    </CardText>
+                                </CardBody>
+                            </Card>
                         ))}
-                    </ListGroup>
-
+                    </CardColumns>
                 </MobileView>
 
-
-                {/* <div className='table-wrapper'>
-                    <div className='table-header'>
-                        {headers.map((h, i) => <span key={i}>{h}</span>)}
-                    </div>
-
-                    {this.props.meetings.map(m => (
-                        <div className='table-row' key={m.id}>
-                            <span className='mobile-only'>{tableHeaders.THERAPIST}</span>
-                            <span>{m.therapist}</span>
-                            <span className='mobile-only'>{tableHeaders.PATIENT}</span>
-                            <span>{m.patient}</span>
-                            <span className='mobile-only'>{tableHeaders.DEPARTMENT}</span>
-                            <span>{m.department.name}</span>
-                            <span className='mobile-only'>{tableHeaders.ATTENDEE}</span>
-                            <span>{m.attendee.name}</span>
-                            <span className='mobile-only'>{tableHeaders.REPORT_DATE}</span>
-                            <span>{stringUtils.getDateString(new Date(m.reportDate))}</span>
-                        </div>
-                    ))}
-                </div> */}
-            </CustomLoader>
+            </CustomLoader >
         );
     }
 }
